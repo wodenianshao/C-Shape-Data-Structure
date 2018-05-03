d4fe6492ab7b8ddba6a46e1728d3ce0252d2353e
hxd <wodenianshao@163.com>
2018-05-03 16:15:33 +0800

    抽象类
    抽象类

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Magpie mp = new Magpie();
+            mp.Fly();
+
+            //可以使用抽象类申明对象，但是不可以实例化对象，需要通过子类去实现
+            Bird bd = new Magpie();
+            bd.Fly();
+            Console.ReadKey();
+        }
+    }
+}
