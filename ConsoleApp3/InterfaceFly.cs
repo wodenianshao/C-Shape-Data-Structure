8098a940645d34a6da42283f83b799f1a9103b09
hxd <wodenianshao@163.com>
2018-05-03 18:07:39 +0800

    接口的应用
    接口的应用

---
+++ ConsoleApp3/InterfaceFly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    interface InterfaceFly
+    {
+        //接口中方法不需要访问修饰符
+        //接口中方法没有实现体
+        void Fly();
+        void Method1();
+        void Method2();
+    }
+}
---
+++ ConsoleApp3/SmallBird.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class SmallBird:InterfaceFly
+    {
+        public void Fly()
+        {
+
+        }
+        public void Method1()
+        {
+
+        }
+        public void Method2()
+        {
+
+        }
+    }
+}
